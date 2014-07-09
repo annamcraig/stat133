bf43e93048b23fc6b8c453aeb54fd51949563842
Xi Chen <xichen@Xis-MacBook-Pro.local>
2014-07-08 17:51:17 -0700

    incremental

--- hw3/ex5.r
+++ hw3/ex5.r
@@ -17,6 +17,8 @@ load('ex5-tests.rda')
 firstLast <- function(data) {
 
     # your code here *
+  len = length(data[, 1])
+  data[c(1:5, (len-4):len), ]
 }
 
 tryCatch(checkEquals(first.last.t, firstLast(iris)), error=function(err)
@@ -36,6 +38,7 @@ tryCatch(checkEquals(first.last.t, firstLast(iris)), error=function(err)
 npRatio <- function(data) {
 
     # your code here *
+  dim(data)[2] / dim(data)[1]
 }
 
 tryCatch(checkEquals(np.ratio.t, npRatio(iris)), error=function(err)
@@ -55,8 +58,15 @@ tryCatch(checkEquals(np.ratio.t, npRatio(iris)), error=function(err)
 numericSummary <- function(data) {
 
     # your code here **
+  n.data = unname(data[, sapply(X=data[1,], FUN=is.numeric)])
+  qs = apply(X=n.data, MARGIN=c(2), FUN=quantile)
+  rbind(
+    qs[1:3, ],
+    apply(X=n.data, FUN=mean, MARGIN=c(2)),
+    qs[4:5, ]
+  )
 }
-
+# numerical issue?
 tryCatch(checkEquals(numeric.summary.t, unname(numericSummary(ex5.test1))),
          error=function(err) errMsg(err))
 
@@ -73,6 +83,7 @@ tryCatch(checkEquals(numeric.summary.t, unname(numericSummary(ex5.test1))),
 getClass <- function(data) {
 
     # your code here *
+  sapply(X=data[1,], FUN=class)
 }
 
 tryCatch(checkEquals(get.class.t, unname(getClass(ex5.test1))), error=function(err)
